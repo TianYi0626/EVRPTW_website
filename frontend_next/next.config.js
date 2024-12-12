f9a30b775c832fc9885838ff300fce1c7284a643
tia <tianyi21@mails.tsinghua.edu.cn>
2024-12-12 18:42:08 +0800

    fix: adjustment for environment

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -1,7 +1,9 @@
+const { config } = require('localforage');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // TODO Start: [Student] Enable standalone build
-    output: undefined,
+    output: "standalone",
     // TODO End
     reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
     swcMinify: true,
@@ -10,11 +12,12 @@ const nextConfig = {
         return [{
             source: "/api/:path*",
             // TODO Start: [Student] Change to standard backend URL
-            destination: "http://127.0.0.1:8001/:path*",
+            destination: "https://127.0.0.1:8001/api/:path*",
             // TODO End
         }];
     }
 };
+
 
 // eslint-disable-next-line no-undef
 module.exports = nextConfig;
